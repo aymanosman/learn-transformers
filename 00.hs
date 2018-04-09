e354196ff5855b61a2be5bfe2462802e789b0abf
Ayman Osman <aymano.osman@gmail.com>
2018-04-09 17:06:25 +0100

    init

---
+++ 00.hs
@@ -0,0 +1,62 @@
+import           Control.Monad             (mzero)
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Maybe
+
+
+main :: IO ()
+main =
+    -- program >>= print
+    run programT >>= print
+
+
+-- Without Transformers
+
+prompt
+    :: String
+    -> IO (Maybe String)
+program
+    :: IO (Maybe (String, String))
+
+prompt s = do
+    putStr s
+    l <- getLine
+    if l == "END"
+       then return Nothing
+       else return $ Just l
+
+program = do
+    a <- prompt "firstname: "
+    case a of
+      Nothing -> return Nothing
+      Just a' -> do
+        b <- prompt "lastname: "
+        case b of
+          Nothing -> return Nothing
+          Just b' -> return $ Just (a', b')
+
+
+-- Using Transformers
+
+run :: Prog a -> IO (Maybe a)
+run = runMaybeT
+
+type Prog = MaybeT IO
+
+promptT
+    :: String
+    -> Prog String
+programT
+    :: Prog (String, String)
+
+promptT s = do
+    l <- liftIO $ do
+        putStr s
+        getLine
+    if l == "END"
+       then mzero
+       else return l
+
+programT = do
+    a <- promptT "firstname: "
+    b <- promptT "lastname: "
+    return (a, b)
